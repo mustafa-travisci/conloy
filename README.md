7bc347bb0e82ce01e51443d0cbb4055ddd99a73e
ElitKrit <dasskrit@gmail.com>
2020-08-16 17:53:26 -0400

    Update

--- README.md
+++ README.md
@@ -1,26 +1,70 @@
-# What is this?
+# conloy
 
-Make string conversion and if return statements a piece of cake.
+![NPM](https://img.shields.io/npm/l/conloy?color=blue)
+[![install size](https://packagephobia.com/badge?p=conloy)](https://packagephobia.com/result?p=conloy)
+![GitHub package.json version](https://img.shields.io/github/package-json/v/ElitKrit/conloy)
+![Travis (.com)](https://img.shields.io/travis/com/ElitKrit/conloy)
+![npm](https://img.shields.io/npm/dw/conloy?color=blue)
+[![FOSSA Status](https://app.fossa.com/api/projects/git%2Bgithub.com%2FElitKrit%2Fconloy.svg?type=shield)](https://app.fossa.com/projects/git%2Bgithub.com%2FElitKrit%2Fconloy?ref=badge_shield)
 
-# Installation
+A lightweight solution to the tedious process of writing if return statements.
 
-`npm i convert --save`
+## Installation
+
+`npm i conloy`
 
 Then...
 
+Add "type": "module" to your package.json or set the type attribute of your script tag to "module". 
+
 ```
-import { convert } from 'convert';
+import conloy from 'conloy';
+const { convert } = conloy;
 
-Convert(x, y, target, location, caseSensitivity, crossConversion);
-```
+convert(x, y, target, caseSensitivity, crossConversion);
+```    
+### Options
 
-## Options
-
-Convert supports 6 parameters, 3 of which are optional:
+This function supports 5 parameters, 2 of which are optional:
 
 * *x* - _The text that will be converted_ (Supports arrays)
-* *y* - _The text that x will be converted to_ (Supports arrays, returned or printed in document if location is not mentioned) 
+* *y* - _The text that x will be converted to_ (Supports arrays) 
 * *target* - _Where to get x from_ 
-* *location* - _Where y will be printed after checking the value of target_ (optional)
-* *caseSensitivity* - _boolean | if is case-sensitive_ (optional, defaults to false)
-* *crossConversion* _boolean | if y converts to x_ (optional, defaults to false) 
+* *caseSensitivity* - _boolean | is it case-sensitive_ (optional, defaults to false)
+* *crossConversion* _boolean | does y convert to x_ (optional, defaults to false) 
+
+## Example
+
+### Without conloy 
+
+```
+if (number === 0) {
+    return 'zero';
+} else if (number === 1) {
+    return 'one';
+}  else if (number === 2) {
+    return 'two';
+} else if (number === 3) {
+    return 'three';
+} else if (number === 4) {
+    return 'four';
+} else if (number === 5) {
+    return 'five';
+} else if (number === 6) {
+    return 'six';
+} else if (number === 7) {
+    return 'seven';
+} else if (number === 8) {
+    return 'eight';
+} else if (number === 9) {
+    return 'nine';
+}
+```
+### With conloy
+
+```
+import conloy from 'conloy';
+const { convert } = conloy;
+
+convert([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'], number);
+```
--- convert.js
+++ convert.js
@@ -1,245 +1,80 @@
-function Convert(x, y, target, location, caseSensitivity, crossConversion) {
-    if (location) {
-        if (Array.isArray(location)) {
-            if ((caseSensitivity === false && crossConversion === false) || (typeof caseSensitivity == 'undefined' && typeof crossConversion == 'undefined')) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target.toUpperCase() === x[i].toUpperCase()) {
-                            location.push(y[i]);
-                        }
-                    }
-                } else {
-                    if (target.toUpperCase() === x.toUpperCase()) {
-                        location.push(y);
-                    }
-                }
-            } else if ((caseSensitivity === true && crossConversion === false) || (caseSensitivity === true && typeof crossConversion == 'undefined')) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target === x[i]) {
-                            location.push(y[i]);
-                        }
-                    }
-                } else {
-                    if (target === x) {
-                        location.push(y);
-                    }
-                }
-            } else if ((caseSensitivity === false && crossConversion === true) || (typeof caseSensitivity == 'undefined' && crossConversion === true)) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target.toUpperCase() === x[i].toUpperCase()) {
-                            location.push(y[i]);
-                        } else if (target.toUpperCase() === y[i].toUpperCase()) {
-                            location.push(x[i]);
-                        }
-                    }
-                } else {
-                    if (target.toUpperCase() === x.toUpperCase()) {
-                        location.push(y);
-                    } else if (target.toUpperCase() === y.toUpperCase()) {
-                        location.push(x);
-                    }
-                }
-            } else if (caseSensitivity === true && crossConversion === true) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target === x[i]) {
-                            location.push(y[i]);
-                        } else if (target === y[i]) {
-                            location.push(x[i]);
-                        }
-                    }
-                } else {
-                    if (target === x) {
-                        location.push(y);
-                    } else if (target === y) {
-                        location.push(x);
-                    }
-                }
-            } else {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target.toUpperCase() === x[i].toUpperCase()) {
-                            location.push(y[i]);
-                        }
-                    }
-                } else {
-                    if (target.toUpperCase() === x.toUpperCase()) {
-                        location.push(y);
-                    }
+function convert(x, y, target, caseSensitivity = false, crossConversion = false) {
+    const xtu = x.toString().toUpperCase();
+    const ytu = y.toString().toUpperCase();
+    const targettu = target.toString().toUpperCase();
+    if (caseSensitivity === false && crossConversion === false) {
+        if (Array.isArray(x) && Array.isArray(y) && x.length === y.length) {
+            for (let i = 0; i < x.length; i++) {
+                if (targettu === x[i].toString()
+                    .toUpperCase()) {
+                    return y[i];
                 }
             }
         } else {
-            if ((caseSensitivity === false && crossConversion === false) || (typeof caseSensitivity == 'undefined' && typeof crossConversion == 'undefined')) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target.toUpperCase() === x[i].toUpperCase()) {
-                            location.innerHTML = y[i];
-                            location = y[i];
-                        }
-                    }
-                } else {
-                    if (target.toUpperCase() === x.toUpperCase()) {
-                        location.innerHTML = y;
-                        location = y;
-                    }
+            if (targettu === xtu) {
+                return y;
+            }
+        }
+    } else if (caseSensitivity === true && crossConversion === false) {
+        if (Array.isArray(x) && Array.isArray(y) && x.length === y.length) {
+            for (let i = 0; i < x.length; i++) {
+                if (target === x[i]) {
+                    return y[i];
                 }
-            } else if ((caseSensitivity === true && crossConversion === false) || (caseSensitivity === true && typeof crossConversion == 'undefined')) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target === x[i]) {
-                            location.innerHTML = y[i];
-                            location = y[i];
-                        }
-                    }
-                } else {
-                    if (target === x) {
-                        location.innerHTML = y;
-                        location = y;
-                    }
+            }
+        } else {
+            if (target === x) {
+                return y;
+            }
+        }
+    } else if (caseSensitivity === false && crossConversion === true) {
+        if (Array.isArray(x) && Array.isArray(y) && x.length === y.length) {
+            for (let i = 0; i < x.length; i++) {
+                if (targettu === x[i].toString()
+                    .toUpperCase()) {
+                    return y[i];
+                } else if (targettu === y[i].toString()
+                    .toUpperCase()) {
+                    return x[i];
                 }
-            } else if ((caseSensitivity === false && crossConversion === true) || (typeof caseSensitivity == 'undefined' && crossConversion === true)) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target.toUpperCase() === x[i].toUpperCase()) {
-                            location.innerHTML = y[i];
-                            location = y[i];
-                        } else if (target.toUpperCase() === y[i].toUpperCase()) {
-                            location.innerHTML = x[i];
-                            location = x[i];
-                        }
-                    }
-                } else {
-                    if (target.toUpperCase() === x.toUpperCase()) {
-                        location.innerHTML = y;
-                        location = y;
-                    } else if (target.toUpperCase() === y.toUpperCase()) {
-                        location.innerHTML = x;
-                        location = x;
-                    }
+            }
+        } else {
+            if (targettu === xtu) {
+                return y;
+            } else if (targettu === ytu) {
+                return x;
+            }
+        }
+    } else if (caseSensitivity === true && crossConversion === true) {
+        if (Array.isArray(x) && Array.isArray(y) && x.length === y.length) {
+            for (let i = 0; i < x.length; i++) {
+                if (target === x[i]) {
+                    return y[i];
+                } else if (target === y[i]) {
+                    return x[i];
                 }
-            } else if (caseSensitivity === true && crossConversion === true) {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target === x[i]) {
-                            location.innerHTML = y[i];
-                            location = y[i];
-                        } else if (target === y[i]) {
-                            location.innerHTML = x[i];
-                            location = x[i];
-                        }
-                    }
-                } else {
-                    if (target === x) {
-                        location.innerHTML = y;
-                        location = y;
-                    } else if (target === y) {
-                        location.innerHTML = x;
-                        location = x;
-                    }
-                }
-            } else {
-                if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                    for (i = 0; i < x.length; i++) {
-                        if (target.toUpperCase() === x[i].toUpperCase()) {
-                            location.innerHTML = y[i];
-                            location = y[i];
-                        }
-                    }
-                } else {
-                    if (target.toUpperCase() === x.toUpperCase()) {
-                        location.innerHTML = y;
-                        location = y;
-                    }
-                }
+            }
+        } else {
+            if (target === x) {
+                return y;
+            } else if (target === y) {
+                return x;
             }
         }
     } else {
-        if ((caseSensitivity === false && crossConversion === false) || (typeof caseSensitivity == 'undefined' && typeof crossConversion == 'undefined')) {
-            if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                for (i = 0; i < x.length; i++) {
-                    if (target.toUpperCase() === x[i].toUpperCase()) {
-                        document.write(y[i]);
-                        return y[i];
-                    }
-                }
-            } else {
-                if (target.toUpperCase() === x.toUpperCase()) {
-                    document.write(y);
-                    return y;
-                }
-            }
-        } else if ((caseSensitivity === true && crossConversion === false) || (caseSensitivity === true && typeof crossConversion == 'undefined')) {
-            if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                for (i = 0; i < x.length; i++) {
-                    if (target === x[i]) {
-                        document.write(y[i]);
-                        return y[i];
-                    }
-                }
-            } else {
-                if (target === x) {
-                    document.write(y);
-                    return y;
-                }
-            }
-        } else if ((caseSensitivity === false && crossConversion === true) || (typeof caseSensitivity == 'undefined' && crossConversion === true)) {
-            if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                for (i = 0; i < x.length; i++) {
-                    if (target.toUpperCase() === x[i].toUpperCase()) {
-                        document.write(y[i]);
-                        return y[i];
-                    } else if (target.toUpperCase() === y[i].toUpperCase()) {
-                        document.write(x[i]);
-                        return x[i];
-                    }
-                }
-            } else {
-                if (target.toUpperCase() === x.toUpperCase()) {
-                    document.write(y);
-                    return y;
-                } else if (target.toUpperCase() === y.toUpperCase()) {
-                    document.write(x);
-                    return x;
-                }
-            }
-        } else if (caseSensitivity === true && crossConversion === true) {
-            if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                for (i = 0; i < x.length; i++) {
-                    if (target === x[i]) {
-                        document.write(y[i]);
-                        return y[i];
-                    } else if (target === y[i]) {
-                        document.write(x[i]);
-                        return x[i];
-                    }
-                }
-            } else {
-                if (target === x) {
-                    document.write(y);
-                    return y;
-                } else if (target === y) {
-                    document.write(x);
-                    return x;
+        if (Array.isArray(x) && Array.isArray(y) && x.length === y.length) {
+            for (let i = 0; i < x.length; i++) {
+                if (targettu === x[i].toString()
+                    .toUpperCase()) {
+                    return y[i];
                 }
             }
         } else {
-            if (Array.isArray(x) && Array.isArray(y) && x.length == y.length) {
-                for (i = 0; i < x.length; i++) {
-                    if (target.toUpperCase() === x[i].toUpperCase()) {
-                        document.write(y[i]);
-                        return y[i];
-                    }
-                }
-            } else {
-                if (target.toUpperCase() === x.toUpperCase()) {
-                    document.write(y);
-                    return y;
-                }
+            if (targettu === xtu) {
+                return y;
             }
         }
     }
 }
 
-module.exports.Convert = convert;
+module.exports.convert = convert;
